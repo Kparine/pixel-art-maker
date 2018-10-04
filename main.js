46b8070f91815be7883c322e13411604ad8ebe38
Kevin Parine <kparine314@gmail.com>
2018-10-04 12:55:31 -0700

    done for now

--- main.js
+++ main.js
@@ -1,32 +1,24 @@
-function makeGrid() {
 
-let grid = document.querySelector('#grid')
+function canvas() {
+    let grid = document.querySelector('#grid')
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 2046; i++) {     
         let cell = document.createElement('div')
+        cell.classList.add('pixel')
         grid.appendChild(cell)
-    
     }
 }
+canvas()
 
 function addClickEvents() {
-    let cells = document.getElementsByClassName('cell')
+    let cells = document.getElementsByClassName('pixel')
+    
     for (let i = 0; i < cells.length; i++) {
       cells[i].addEventListener('click',  function(event) {
+        let colorSelector = document.querySelector('#colorSelector')
         let clickedCell = event.target
-        clickedCell.style.backgroundColor = selectedColor
+        clickedCell.style.backgroundColor = colorSelector.value
       })
     }
-  }
-  
-
-
-
-// 1. Create a small, 2x2 grid canvas made up of white, square `div` tags with a border.
-// 1. Add an event listener to each `div` so when clicked the background turns red.
-// 1. Create a small palette of two colors (e.g. red and blue) below the canvas using more `div` tags.
-// 1. Add an event listener to these `div` tags so when clicked the brush color is saved.
-// 1. Expand the dimensions of the pixel canvas.
-// 1. Expand the palette with more colors. (i.e. red, orange, yellow, green, blue, purple, brown, gray, black, white, etc.)
-// 1. Expand the palette with a brush color indicator.
-// 1. Improve the look and feel of the canvas and color palette. Be careful not to break your program's behavior!
+}
+addClickEvents()
